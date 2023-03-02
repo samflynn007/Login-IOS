5e7caf1c72d2e8c60d964a82aee4b9e470736f82
Venkatesh nellore <51104553+samflynn007@users.noreply.github.com>
2023-03-02 12:20:21 +0530

    Add files via upload

---
+++ Login/AisleLogin/OTPViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OTPViewController.swift
+//  AisleLogin
+//
+//  Created by venky N on 02/03/23.
+//
+
+import UIKit
+
+class OTPViewController: UIViewController {
+    
+    
+    @IBOutlet weak var OTPTextField: UITextField!
+    
+    @IBOutlet weak var continueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Login/AisleLogin/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AisleLogin
+//
+//  Created by venky N on 02/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mobileNumberTextField: UITextField!
+    
+    @IBOutlet weak var continueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view
+        
+            
+    }
+
+
+    @IBAction func onTapContinue(_ sender: Any) {
+        
+        
+        
+    }
+}
+
